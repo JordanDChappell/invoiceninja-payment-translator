0ffe7ec4b65574631f8a4df294f405e9ea1799ab
Jordan Chappell <me@jordanchappell.com>
2026-03-05 22:13:20 +1100

    devops(scripts): tweaking notify script to help debug

--- devops/scripts/notifytools.sh
+++ devops/scripts/notifytools.sh
@@ -4,6 +4,6 @@ DIR="$1"
 inotifywait -m -q -e create -e moved_to --format "%w%f" "$DIR" | while read -r NEW_FILE
 do
   echo "File discovered: $NEW_FILE"
-  echo "Running node script"
+  echo "Running node script: /app/index.js $2 -i $NEW_FILE ${@:3}"
   node /app/index.js $2 -i $NEW_FILE ${@:3}
 done
